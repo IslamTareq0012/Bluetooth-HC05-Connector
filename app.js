51518d3c6ee1fdb65e9b09f2afa9bde275421404
Islam-tareq <islam_elmasry@hotmail.com>
2021-12-27 08:18:23 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+router.post('/connect', function (req, res) {
+    res.render('connect');
+
+    const bluetooth = require('node-bluetooth');
+    const device = new bluetooth.DeviceINQ();
+
+    // Find devices
+    device
+        .on('finished', console.log.bind(console, 'finished'))
+        .on('found', function found(address, name) {
+            console.log('Found: ' + address + ' with name ' + name);
+
+            // We know our Arduino bluetooth module is called 'HC-05', so we only want to connect to that.
+            if (name === 'HC-05') {
+
+                // find serial port channel
+                device.findSerialPortChannel(address, function (channel) {
+                    console.log('Found channel for serial port on %s: ', name, channel);
+
+                    // make bluetooth connect to remote device
+                    bluetooth.connect(address, channel, function (err, connection) {
+                        if (err) return console.error(err);
+
+                        // This is some example code from the library for writing, customize as you wish.
+                        connection.delimiter = Buffer.from('/n', 'utf-8');
+                        connection.on('data', (buffer) => {
+                            io.on('connection', (socket) => {
+                                socket.broadcast.emit('received message: ', buffer.toString());
+                            });
+                        });
+                    });
+                });
+            }
+        });
+    device.scan();
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
